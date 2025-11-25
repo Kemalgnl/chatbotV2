41bbadf0e8d58d0ff905a0433d8931dc254a9916
Kemal Gınalı <kemal.ginali@outlook.com>
2025-11-25 12:56:15 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -56,7 +56,7 @@ def uploaddocument(file: UploadFile = File(...)):
    
     
 async def restart_server():
-    url = "https://sacdokulmesitahmini-8.onrender.com" 
+    url = "https://chatbotprojesi-8.onrender.com" 
     while True:
         try:
             async with httpx.AsyncClient() as istemci:
